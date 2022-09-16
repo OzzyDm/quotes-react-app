9d7010313cb98ebd3a44078ec38f72a6b767018d
unknown <oguzhandemiraslan@gmail.com>
2022-09-16 15:00:20 -0400

    routes added

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,5 @@
+const AllQuotes = () => {
+  return <h1>AllQuote page</h1>;
+};
+
+export default AllQuotes;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const QuoteDetail = () => {
+  return <h1>Detail page</h1>;
+};
+
+export default QuoteDetail;
